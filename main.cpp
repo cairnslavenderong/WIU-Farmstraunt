35296f7cb45307fd57c1f393b5ffb95b028e9c00
Regero <107737505+ReginaTWRR@users.noreply.github.com>
2025-08-19 09:09:59 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#include "Windows.h"
+#include "board.h"
+#include "game.h"
+using std::cout;
+using std::endl;
+int main() {
+	game testGame;
+	board testBoard;
+	testGame.doIntro();
+	Sleep(500);
+	while (testGame.endGame() == false) {
+		cout << endl;
+		if (testGame.getTurn() == 1 or testGame.getTurn() % 10 == 0) {
+			cout << "**********" << endl;
+			cout << "Time: " << testGame.getTime() << testGame.getDay() << endl;
+			cout << "Turn: " << testGame.getTurn() << endl;
+			cout << "Money: $" << testGame.getMoney() << endl;
+			cout << "**********" << endl;
+		}
+		cout << endl;
+		cout << "Turn " << testGame.getTurn() << endl;
+		testBoard.printBoard();
+		testBoard.move();
+		testGame.setTurn();
+	}
+	Sleep(500);
+	testGame.doOutro();
+	_CrtDumpMemoryLeaks();
+	return 0;
+}
