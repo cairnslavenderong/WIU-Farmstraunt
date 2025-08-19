f60c9324e79c4e58a958e611be46fe546e745d67
Regero <107737505+ReginaTWRR@users.noreply.github.com>
2025-08-19 12:00:39 +0800

    Add files via upload

---
+++ game.h
@@ -0,0 +1,32 @@
+#pragma once
+class game {
+private:
+	int turn;
+	int day;
+	int night;
+	char time;
+	int money;
+	bool raining;
+public:
+	game();
+	~game();
+
+	// getters
+	int getTurn() const;
+	int getDay() const;
+	int getNight() const;
+	char getTime() const;
+	int getMoney() const;
+	bool getRaining() const;
+	// setters
+	void setTurn();
+	void setDay();
+	void setNight();
+	void setTime();
+	void setMoney(int passedIncrement);
+	void setRaining(bool passedWeather);
+
+	void doIntro();
+	void doOutro();
+	bool endGame() const;
+};
